84a3a9467255b7a89b22cce99110776c6de32540
Dmytro Kovalenko <kovalenko.d.y@gmai.com>
2017-04-14 15:35:29 +0200

    Created template for further development

---
+++ LongCalculator/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace LongCalculator
+{
+    partial class divBtn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputNumber1 = new System.Windows.Forms.TextBox();
+            this.inputNumber2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.appTitle = new System.Windows.Forms.Label();
+            this.inputLbl = new System.Windows.Forms.Label();
+            this.operationsLbl = new System.Windows.Forms.Label();
+            this.sumBtn = new System.Windows.Forms.Button();
+            this.diffBtn = new System.Windows.Forms.Button();
+            this.multBtn = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.resultLbl = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // inputNumber1
+            // 
+            this.inputNumber1.Location = new System.Drawing.Point(90, 90);
+            this.inputNumber1.Name = "inputNumber1";
+            this.inputNumber1.Size = new System.Drawing.Size(750, 20);
+            this.inputNumber1.TabIndex = 0;
+            // 
+            // inputNumber2
+            // 
+            this.inputNumber2.Location = new System.Drawing.Point(90, 130);
+            this.inputNumber2.Name = "inputNumber2";
+            this.inputNumber2.Size = new System.Drawing.Size(750, 20);
+            this.inputNumber2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Number 2";
+            // 
+            // appTitle
+            // 
+            this.appTitle.AutoSize = true;
+            this.appTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.appTitle.Location = new System.Drawing.Point(305, 9);
+            this.appTitle.Name = "appTitle";
+            this.appTitle.Size = new System.Drawing.Size(319, 31);
+            this.appTitle.TabIndex = 4;
+            this.appTitle.Text = "Long Integer Calculator";
+            // 
+            // inputLbl
+            // 
+            this.inputLbl.AutoSize = true;
+            this.inputLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputLbl.Location = new System.Drawing.Point(413, 63);
+            this.inputLbl.Name = "inputLbl";
+            this.inputLbl.Size = new System.Drawing.Size(93, 24);
+            this.inputLbl.TabIndex = 5;
+            this.inputLbl.Text = "Input Data";
+            // 
+            // operationsLbl
+            // 
+            this.operationsLbl.AutoSize = true;
+            this.operationsLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.operationsLbl.Location = new System.Drawing.Point(413, 181);
+            this.operationsLbl.Name = "operationsLbl";
+            this.operationsLbl.Size = new System.Drawing.Size(102, 24);
+            this.operationsLbl.TabIndex = 6;
+            this.operationsLbl.Text = "Operations";
+            // 
+            // sumBtn
+            // 
+            this.sumBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sumBtn.Location = new System.Drawing.Point(90, 220);
+            this.sumBtn.Name = "sumBtn";
+            this.sumBtn.Size = new System.Drawing.Size(150, 50);
+            this.sumBtn.TabIndex = 7;
+            this.sumBtn.Text = "+";
+            this.sumBtn.UseVisualStyleBackColor = true;
+            // 
+            // diffBtn
+            // 
+            this.diffBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.diffBtn.Location = new System.Drawing.Point(284, 220);
+            this.diffBtn.Name = "diffBtn";
+            this.diffBtn.Size = new System.Drawing.Size(150, 50);
+            this.diffBtn.TabIndex = 8;
+            this.diffBtn.Text = "-";
+            this.diffBtn.UseVisualStyleBackColor = true;
+            // 
+            // multBtn
+            // 
+            this.multBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.multBtn.Location = new System.Drawing.Point(494, 220);
+            this.multBtn.Name = "multBtn";
+            this.multBtn.Size = new System.Drawing.Size(150, 50);
+            this.multBtn.TabIndex = 9;
+            this.multBtn.Text = "*";
+            this.multBtn.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(690, 220);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 50);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // resultLbl
+            // 
+            this.resultLbl.AutoSize = true;
+            this.resultLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLbl.Location = new System.Drawing.Point(434, 303);
+            this.resultLbl.Name = "resultLbl";
+            this.resultLbl.Size = new System.Drawing.Size(62, 24);
+            this.resultLbl.TabIndex = 11;
+            this.resultLbl.Text = "Result";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(90, 330);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(750, 65);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // divBtn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 421);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.resultLbl);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.multBtn);
+            this.Controls.Add(this.diffBtn);
+            this.Controls.Add(this.sumBtn);
+            this.Controls.Add(this.operationsLbl);
+            this.Controls.Add(this.inputLbl);
+            this.Controls.Add(this.appTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputNumber2);
+            this.Controls.Add(this.inputNumber1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "divBtn";
+            this.Text = "Long Integer Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputNumber1;
+        private System.Windows.Forms.TextBox inputNumber2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label appTitle;
+        private System.Windows.Forms.Label inputLbl;
+        private System.Windows.Forms.Label operationsLbl;
+        private System.Windows.Forms.Button sumBtn;
+        private System.Windows.Forms.Button diffBtn;
+        private System.Windows.Forms.Button multBtn;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label resultLbl;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
